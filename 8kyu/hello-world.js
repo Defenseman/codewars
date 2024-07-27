b1a78d6995c3c15b18c422d65951d9f868153559
Defenseman <worknesterov@yandex.kz>
2024-07-27 21:16:43 +0500

    hello world!

---
+++ 8kyu/hello-world.js
@@ -0,0 +1,15 @@
+//❓ DESCRIPTION:
+// --------keyword "function"
+//       |       ----your function name
+//       |       |    ---if needed, some arguments will appear in parentheses
+//       |       |    |
+//     function myfunc(){  ---------your function code will starting with "{"
+//       //you should type your code here
+//     }----------------------------ending with "}"
+
+// ✅ SOLUTION:
+function helloWorld()  {
+    let str = 'Hello World!'
+    console.log(str)
+}
+helloWorld()
